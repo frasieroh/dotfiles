cfe53cd29855bc4610b5fd1616b0f9a8330feb11
Oscar Frasier <frasieroh@gmail.com>
2021-11-21 02:20:03 -0500

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Install oh-my-zsh and antigen
+export CHSH=no
+export RUNZSH=no
+export KEEP_ZSHRC=yes
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -L git.io/antigen > $HOME/.antigen.zsh
 
 # Install vim plugins
 vim +PlugInstall +qall
 
-# Add symbolic links to home directory
+# Add symbolic links to home directory (admittedly this is crude)
+rm $HOME/.tmux.conf
+rm $HOME/.p10k.zsh
+rm $HOME/.zshrc
+rm $HOMR/.vimrc
 ln -s .tmux.conf $HOME/.tmux.conf
 ln -s .p10k.zsh $HOME/.p10k.zsh
 ln -s .zshrc $HOME/.zshrc
