1ab99509f1054d1c3752f567621133d826cd66e2
Oscar Frasier <ofrasier@arista.com>
2022-07-02 13:07:37 -0700

    fix packer bootstrap

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 -- Plugins
-local packer = require('packer')
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
+local packer = require('packer')
 packer.startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'NLKNguyen/papercolor-theme'
