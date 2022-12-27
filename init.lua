5bee8c029c9daa93833cb4a4c307c15cc5c772bd
Oscar Frasier <frasieroh@gmail.com>
2022-12-26 20:30:20 -0500

    Switch to lazy.nvim in a vain attempt to get LSP to start working (which it did)

--- init.lua
+++ init.lua
@@ -1,256 +1,302 @@
--- Install packer
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-	vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+-- Install folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
+vim.opt.runtimepath:prepend(lazypath)
 
 -- Plugins
-local packer = require('packer') packer.startup(function(use)
-use { 'wbthomason/packer.nvim',
-	tag = '*',
-}
+local plugins = {
+	-- LSP and related plugins
+	{
+		"neovim/nvim-lspconfig",
+		-- Don't load until mason-lspconfig requires this
+		-- It requires itself to be set up first
+		lazy = true,
+	},
 
-use { 'NLKNguyen/papercolor-theme',
-	tag = '*',
-	config = function()
-		vim.opt.background = "dark"
-		vim.cmd("colorscheme PaperColor")
-	end,
-}
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		-- Don't load until mason-null-ls includes this
+		-- It requires itself to be set up first
+		lazy = true,
+	},
 
-use { 'christoomey/vim-tmux-navigator',
-	tag = '*',
-}
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require('mason').setup {}
+		end,
+	},
 
-use { 'nvim-telescope/telescope.nvim',
-	tag = '*',
-	requires = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin_config = function(initial_mode)
-			return {
-				theme = "ivy",
-				initial_mode = initial_mode,
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local mason_lspconfig = require('mason-lspconfig')
+			mason_lspconfig.setup {}
+			mason_lspconfig.setup_handlers {
+				-- Default setup handler for LSP servers
+				function(server_name)
+					return require('lspconfig')[server_name].setup {}
+				end,
+				-- Add manual setup here. Example:
+				-- [ "clangd" ] = function() {
+				-- ...
+				-- end,
 			}
-		end
-		require('telescope').setup {
-			pickers = {
-				live_grep = builtin_config("insert"),
-				file_browser = builtin_config("normal"),
-				lsp_definitions = builtin_config("normal"),
-				lsp_type_definitions = builtin_config("normal"),
-				lsp_references = builtin_config("normal"),
-				diagnostics = builtin_config("normal"),
-			},
-			-- extensions = {
-			-- 	file_browser = {
-			-- 		theme = "ivy",
-			-- 		initial_mode = "normal",
-			-- 		hijack_netrw = true,
-			-- 		dir_icon = "+",
-			-- 		dir_icon_hl = "Directory",
-			-- 		-- Make ` work as a toggle.
-			-- 		attach_mappings = function(prompt_bufnr, map)
-			-- 			local actions = require('telescope.actions')
-			-- 			local close_file_browser = function()
-			-- 				actions.close(prompt_bufnr)
-			-- 			end
-			-- 			map("n", "`", close_file_browser)
-			-- 			return true
-			-- 		end,
-			-- 	}
-			-- }
-		}
-		vim.api.nvim_set_keymap("n", "[", ":Telescope lsp_definitions<CR>", { noremap = true })
-		vim.api.nvim_set_keymap("n", "<C-[>", ":Telescope lsp_type_definitions<CR>", { noremap = true })
-		vim.api.nvim_set_keymap("n", "]", ":Telescope lsp_references<CR>", { noremap = true })
-		vim.api.nvim_set_keymap("n", "'", ":Telescope diagnostics bufnr=0<CR>", { noremap = true })
-		vim.api.nvim_set_keymap("n", "fg", ":Telescope live_grep<CR>", { noremap = true })
-		-- vim.api.nvim_set_keymap("n", "`", ":Telescope file_browser<CR>", { noremap = true })
-	end,
-}
+		end,
+	},
 
--- I really like this plugin, but it's too buggy right now.
--- use { 'nvim-telescope/telescope-file-browser.nvim',
--- 	tag = '*',
--- 	requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
--- 	after = { 'telescope.nvim' },
--- 	config = function()
--- 		require('telescope').load_extension("file_browser")
--- 	end,
--- }
-
--- Probably replace this with telescope-file-browser when it's more mature
-use { 'kyazdani42/nvim-tree.lua',
-	tag = '*',
-	config = function()
-		require('nvim-tree').setup {
-			renderer = { icons = {
-				symlink_arrow = " > ",
-				padding = "",
-				glyphs = {
-					default = "· ",
-					symlink = "l ",
-					git = {
-						unstaged = "* ",
-						staged = "+ ",
-						unmerged = "? ",
-						renamed = "r ",
-						deleted = "d ",
-						untracked = "x ",
-						ignored = "x ",
-					},
-					folder = {
-						arrow_open = "-",
-						arrow_closed = "+",
-						default = "",
-						open = "",
-						empty = "",
-						empty_open = "",
-						symlink = "",
-						symlink_open = "",
-					},
-				},
-			}},
-		}
-		vim.api.nvim_set_keymap("n", "`", ":NvimTreeToggle<CR>", { noremap = true })
-	end
-}
-
-use { 'nvim-lualine/lualine.nvim',
-	tag = '*',
-	config = function()
-		require('lualine').setup {
-			options = {
-				theme = {
-					normal = {
-						a = { fg = '#C6C6C6', bg = '#3A3A3A' },
-						b = { fg = '#B2B2B2', bg = '#303030' },
-						c = { fg = '#B2B2B2', bg = '#303030' },
-						x = { fg = '#B2B2B2', bg = '#303030' },
-						y = { fg = '#B2B2B2', bg = '#303030' },
-						z = { fg = '#C6C6C6', bg = '#3A3A3A' },
-					},
-					inactive = {
-						a = { fg = '#B2B2B2', bg = '#303030' },
-						b = { fg = '#C6C6C6', bg = '#3A3A3A' },
-						c = { fg = '#C6C6C6', bg = '#3A3A3A' },
-						x = { fg = '#C6C6C6', bg = '#3A3A3A' },
-						y = { fg = '#C6C6C6', bg = '#3A3A3A' },
-						z = { fg = '#B2B2B2', bg = '#303030' },
-					}
-				},
-				icons_enabled = false,
-				section_separators = {left = '', right = ''},
-				component_separators = {left = '', right = ''},
-			},
-			sections = {
-				lualine_a = {'branch'},
-				lualine_b = {'filename'},
-				lualine_c = {''},
-				lualine_x = {''},
-				lualine_y = {'filetype', 'progress', 'location'},
-				lualine_z = {''}
-			},
-			tabline = {
-				lualine_a = {'tabs'},
-				lualine_z = {'buffers'},
-			},
-		}
-	end
-}
-
-use { 'neovim/nvim-lspconfig',
-	tag = '*',
-	-- Lazy loading breaks filetype recognition
-	config = function()
-		local lspconfig = require('lspconfig')
-		lspconfig.clangd.setup {
-			cmd = { 'clangd', '-j=8' },
-		}
-		lspconfig.gopls.setup {}
-		vim.api.nvim_set_keymap("n", "=", ":lua vim.lsp.buf.hover()<CR>", { noremap = true })
-	end
-}
-
-use { 'jose-elias-alvarez/null-ls.nvim',
-	tag = '*',
-	-- Lazy loading breaks filetype recognition
-	requires = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local null_ls = require('null-ls')
-		null_ls.setup {
-			sources = {
-				null_ls.builtins.diagnostics.shellcheck,
-				null_ls.builtins.diagnostics.zsh,
-				null_ls.builtins.diagnostics.pylint,
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local mason_null_ls = require('mason-null-ls')
+			mason_null_ls.setup {
+				automatic_instalation = false,
+				automatic_setup = true,
 			}
-		}
-	end
-}
-
-use { 'nvim-treesitter/nvim-treesitter',
-	tag = '*',
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = {
-				"norg",
-			},
-			highlight = {
-				enable = true,
-			}
-		}
-	end,
-}
-
-use { 'nvim-neorg/neorg',
-	tag = '0.0.12',
-	requires = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
-	after = { 'nvim-treesitter' },
-	config = function()
-		require('neorg').setup {
-			load = {
-				["core.defaults"] = {},
-				["core.norg.qol.toc"] = {},
-				["core.norg.dirman"] = {
-					config = {
-						workspaces = {
-							home = "~/neorg",
+			require("null-ls").setup {}
+			mason_null_ls.setup_handlers {}
+		end,
+	},
+	-- Neorg and related plugins
+	{
+		"nvim-neorg/neorg",
+		-- neorg already lazy loads itself
+		--  lazy = true,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require('neorg').setup {
+				load = {
+					["core.defaults"] = {},
+					["core.norg.qol.toc"] = {},
+					["core.norg.dirman"] = {
+						config = {
+							workspaces = {
+								home = "~/neorg",
+							}
+						},
+						autochdir = true,
+						index = "index.norg",
+					},
+					["core.norg.journal"] = {
+						config = {
+							workspace = "home",
 						}
 					},
-					autochdir = true,
-					index = "index.norg",
+					-- GTD is being rewritten.
+					-- ["core.gtd.base"] = {
+					-- 	config = {
+					-- 		workspace = "home",
+					-- 	}
+					-- },
+				}
+			}
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = true,
+		event = { 'BufRead', 'BufNewFile' },
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = {
+					"norg",
 				},
-				["core.norg.journal"] = {
-					config = {
-						workspace = "home",
-					}
+				highlight = {
+					enable = true,
 				},
-				["core.gtd.base"] = {
-					config = {
-						workspace = "home",
-					}
+				indent = {
+					enable = true,
 				},
 			}
-		}
-	end,
+		end,
+	},
+	-- Other plugins
+	{
+		"tpope/vim-fugitive",
+		lazy = true,
+		event = { 'BufRead', 'BufNewFile' },
+	},
+
+	{
+		"NLKNguyen/papercolor-theme",
+		config = function()
+			vim.opt.background = "dark"
+			vim.cmd("colorscheme PaperColor")
+		end,
+	},
+
+	{
+		"christoomey/vim-tmux-navigator",
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "[", ":Telescope lsp_definitions<CR>", mode = "n" },
+			{ "<C-[>", ":Telescope lsp_type_definitions<CR>", mode = "n" },
+			{ "]", ":Telescope lsp_references<CR>", mode = "n" },
+			{ "'", ":Telescope diagnostics bufnr=0<CR>", mode = "n" },
+			{ "fg", ":Telescope live_grep<CR>", mode = "n" },
+		},
+		config = function()
+			local builtin_config = function(initial_mode)
+				return {
+					theme = "ivy",
+					initial_mode = initial_mode,
+				}
+			end
+			require('telescope').setup {
+				pickers = {
+					live_grep = builtin_config("insert"),
+					file_browser = builtin_config("normal"),
+					lsp_definitions = builtin_config("normal"),
+					lsp_type_definitions = builtin_config("normal"),
+					lsp_references = builtin_config("normal"),
+					diagnostics = builtin_config("normal"),
+				},
+				-- extensions = {
+				-- 	file_browser = {
+				-- 		theme = "ivy",
+				-- 		initial_mode = "normal",
+				-- 		hijack_netrw = true,
+				-- 		dir_icon = "+",
+				-- 		dir_icon_hl = "Directory",
+				-- 		-- Make ` work as a toggle.
+				-- 		attach_mappings = function(prompt_bufnr, map)
+				-- 			local actions = require('telescope.actions')
+				-- 			local close_file_browser = function()
+				-- 				actions.close(prompt_bufnr)
+				-- 			end
+				-- 			map("n", "`", close_file_browser)
+				-- 			return true
+				-- 		end,
+				-- 	}
+				-- }
+			}
+		end,
+	},
+
+	{
+		"kyazdani42/nvim-tree.lua",
+		keys = {
+			{ "`", ":NvimTreeToggle<CR>", mode = "n" },
+		},
+		config = function()
+			require('nvim-tree').setup {
+				renderer = { icons = {
+					symlink_arrow = " > ",
+					padding = "",
+					glyphs = {
+						default = "· ",
+						symlink = "l ",
+						git = {
+							unstaged = "* ",
+							staged = "+ ",
+							unmerged = "? ",
+							renamed = "r ",
+							deleted = "d ",
+							untracked = "x ",
+							ignored = "x ",
+						},
+						folder = {
+							arrow_open = "-",
+							arrow_closed = "+",
+							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							symlink = "",
+							symlink_open = "",
+						},
+					},
+				}},
+			}
+		end,
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require('lualine').setup {
+				options = {
+					theme = {
+						normal = {
+							a = { fg = '#C6C6C6', bg = '#3A3A3A' },
+							b = { fg = '#B2B2B2', bg = '#303030' },
+							c = { fg = '#B2B2B2', bg = '#303030' },
+							x = { fg = '#B2B2B2', bg = '#303030' },
+							y = { fg = '#B2B2B2', bg = '#303030' },
+							z = { fg = '#C6C6C6', bg = '#3A3A3A' },
+						},
+						inactive = {
+							a = { fg = '#B2B2B2', bg = '#303030' },
+							b = { fg = '#C6C6C6', bg = '#3A3A3A' },
+							c = { fg = '#C6C6C6', bg = '#3A3A3A' },
+							x = { fg = '#C6C6C6', bg = '#3A3A3A' },
+							y = { fg = '#C6C6C6', bg = '#3A3A3A' },
+							z = { fg = '#B2B2B2', bg = '#303030' },
+						}
+					},
+					icons_enabled = false,
+					section_separators = {left = '', right = ''},
+					component_separators = {left = '', right = ''},
+				},
+				sections = {
+					lualine_a = {'branch'},
+					lualine_b = {'filename'},
+					lualine_c = {''},
+					lualine_x = {''},
+					lualine_y = {'filetype', 'progress', 'location'},
+					lualine_z = {''}
+				},
+				tabline = {
+					lualine_a = {'tabs'},
+					lualine_z = {'buffers'},
+				},
+			}
+		end,
+	},
+
+	{
+		"tpope/vim-fugitive",
+		lazy = true,
+		event = { 'BufRead', 'BufNewFile' },
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		lazy = true,
+		event = { 'BufRead', 'BufNewFile' },
+	},
+
 }
 
-use { 'tpope/vim-fugitive',
-	tag = '*',
-	event = { 'BufRead', 'BufNewFile' },
-}
-
-use { 'lukas-reineke/indent-blankline.nvim',
-	tag = '*',
-	event = { 'BufRead', 'BufNewFile' },
-}
-
-if packer_bootstrap then
-	packer.sync()
-end
-end)
+require("lazy").setup(plugins, {
+	defaults = {
+		-- Automatically use last semantic version
+		version = "*",
+	}
+})
 
 -- Other settings
 vim.cmd("filetype plugin indent on")
@@ -268,7 +314,8 @@ vim.api.nvim_set_keymap("n", "<S-h>", ":bp<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<S-l>", ":bn<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "˙", ":tabprevious<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "¬", ":tabnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "=", ":lua vim.lsp.buf.hover()<CR>", { noremap = true })
 
 -- Autocommands
-vim.api.nvim_create_autocmd({"BufWrite"}, {pattern="*", callback=vim.lsp.buf.formatting})
+-- vim.api.nvim_create_autocmd({"BufWrite"}, {pattern="*", callback=vim.lsp.buf.formatting})
 
